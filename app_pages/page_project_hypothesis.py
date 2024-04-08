5b20f164cfb6e7221f05afee64e4092dcff5c384
caketaster <caketaster@gmail.com>
2024-04-08 13:37:38 +0000

    Hypothesis page formatted

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -14,14 +14,18 @@ def page_project_hypothesis_body():
 
     st.write("Exploratory data analysis was performed on the image "
              "dataset and two ML models were created to differentiate "
-             "between healthy and infected leaves. \n\n"
-             "The required accuracy was 97%, but both models - "
+             "between healthy and infected leaves. \n\n")
+
+    st.write("The required accuracy was 97%, but both models - "
              "created with different mathematical logic - significantly "
              "outperformed the project requirement. The slightly "
              "more accurate model was then used in the final project to "
-             "be submitted to the client. \n\nA final accuracy rate of "
-             "99.76% accuracy was achieved, proving the hypothesis.\n\n "
-             "This accuracy rate was reached despite significantly "
+             "be submitted to the client.")
+
+    st.write("A final accuracy rate of "
+             "99.76% accuracy was achieved, proving the hypothesis. ")
+             
+    st.write("This accuracy rate was reached despite significantly "
              "resizing the images down due to deployment restrictions. "
              "We can further hypothesise that near-perfect prediction "
              "using an ML model could be achieved with the extra "
